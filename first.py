a8135b00e07b0d488ac1d7dc5fc827bc9830be15
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-20 10:06:36 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+# Condition Statment
+# if_elif_else  (SYNTAX) => Key words
+# TRAFFIC LIGHT
+#EXAMPLE:-
+light = input ("light: ")
+if (light == "red"):
+    print ("stop")
+elif (light == "yellow"):
+    print ("look")
+elif (light == "green"):
+    print ("go")
+else :
+    print ("light is broken")    
